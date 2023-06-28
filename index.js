2245484e9a043f5b7a588e1a9814d3332cf59b73
Ugo Chiori <ugochiori@gmail.com>
2023-06-28 23:20:37 +0100

    server

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
+// import express from "express";
+// import * as dotenv from "dotenv";
+// import cors from "cors";
+// import fetch from "node-fetch";
+
+// const app = express();
+// dotenv.config();
+
+// app.use(cors());
+
+// app.get("/api", async (req, res) => {
+//   res.send("Server is up and running");
+// });
+
+// app.get("/api/maps/places", async (req, res) => {
+//   const { latitude, longitude, radius } = req.query;
+//   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=hospitals&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     res.json(data);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: "Error fetching" });
+//   }
+// });
+
+// app.get("/api/maps/directions", async (req, res) => {
+//   const { origin, destination } = req.query;
+//   const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     res.json(data);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: "Error fetching" });
+//   }
+// });
+
+// app.get("/api/maps/geocode", async (req, res) => {
+//   const { address } = req.query;
+//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     res.json(data);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: "Error fetching" });
+//   }
+// });
+
+// app.get("/api/maps/place/details", async (req, res) => {
+//   const { place_id } = req.query;
+//   const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     res.json(data);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: "Error fetching" });
+//   }
+// });
+
+// app.listen(process.env.PORT || 1500, () => {
+//     console.log(`Server is running on port ${process.env.PORT || 1500}`);
+//     });
 import express from "express";
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 import cors from "cors";
 import fetch from "node-fetch";
 
@@ -8,7 +77,7 @@ dotenv.config();
 
 app.use(cors());
 
-app.get("/api", async (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Server is up and running");
 });
 
@@ -17,11 +86,15 @@ app.get("/api/maps/places", async (req, res) => {
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&type=hospitals&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    res.json(data);
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    } else {
+      throw new Error("Error fetching nearby places");
+    }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error fetching" });
+    console.error(error);
+    res.status(500).json({ error: "Error fetching nearby places" });
   }
 });
 
@@ -30,11 +103,15 @@ app.get("/api/maps/directions", async (req, res) => {
   const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    res.json(data);
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    } else {
+      throw new Error("Error fetching directions");
+    }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error fetching" });
+    console.error(error);
+    res.status(500).json({ error: "Error fetching directions" });
   }
 });
 
@@ -43,11 +120,15 @@ app.get("/api/maps/geocode", async (req, res) => {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    res.json(data);
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    } else {
+      throw new Error("Error fetching geocode");
+    }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error fetching" });
+    console.error(error);
+    res.status(500).json({ error: "Error fetching geocode" });
   }
 });
 
@@ -56,17 +137,21 @@ app.get("/api/maps/place/details", async (req, res) => {
   const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    res.json(data);
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    } else {
+      throw new Error("Error fetching place details");
+    }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Error fetching" });
+    console.error(error);
+    res.status(500).json({ error: "Error fetching place details" });
   }
 });
 
-app.listen(process.env.PORT || 1500, () => {
-    console.log(`Server is running on port ${process.env.PORT || 1500}`);
-    });
-
+const port = process.env.PORT || 1200;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
